f00d3d177edd5bc9dace5315276a7d1d35b2df25
Avnish-kaushik <kaushikavi7002@gmail.com>
2026-01-20 23:20:34 +0530

    Basic JS

--- Javscript/script.js
+++ Javscript/script.js
@@ -4,11 +4,11 @@
 console.log(a);
 // var allows the variable to be redeclared and reassigned and run outside the scope.
 
-{
+
   let b = 15;
   console.log(b);
 
-}
+
 console.log(b);
 // let allows the variable to be reassigned but not redeclared and runs only within the scope.
 
@@ -17,9 +17,18 @@ console.log(b);
 console.log(c);
 }
 
-var name = prompt("What's your name ?");
-console.log(name);
-// prompt allows to input a value from user via browser.
+var username = prompt("What's your name ?");
+// prompt allows to input a value from user via browser. And treat it as a String.
 
-alert("Hello, User");
+alert(`Hello, ${name}!`);
 // alert allows the user to display a message in a pop-up window on the browser.
+
+console.log("Avi");
+console.info("Avi");
+console.warn("Avi");
+console.error("Avi");
+
+// OPERATORS IN JS
+
+// Airthmetic Operator
+// + - * / % **
