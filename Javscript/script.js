bc6d30f34cc4d9d9a98166fe932f449f4272e595
Avnish-kaushik <kaushikavi7002@gmail.com>
2026-02-03 12:03:51 +0530

    Randomness in DOM

--- Javscript/script.js
+++ Javscript/script.js
@@ -445,3 +445,21 @@
 //   console.log("Raang badal gya bhai...");
   
 // })
+
+
+// Math.random() generates a random decimal number between 0 (inclusive) and 1 (exclusive)
+// var a = Math.random() * 100;
+// var b = Math.floor(a);
+// console.log(a);
+// console.log(b);
+
+// var btn = document.querySelector('button')
+// var box = document.querySelector("#box")
+
+btn.addEventListener('click',function(){
+  var c1 = Math.floor(Math.random() * 256) 
+  var c2 = Math.floor(Math.random() * 256)
+  var c3 = Math.floor(Math.random() * 256)
+  box.style.backgroundColor = `rgb(${c1},${c2},${c3})`;
+  
+})
