d54b0974740be6c19bc825ccb7d26b829818c5b1
Avnish-kaushik <kaushikavi7002@gmail.com>
2026-02-04 11:10:57 +0530

    DOM

--- Javscript/script.js
+++ Javscript/script.js
@@ -456,10 +456,34 @@
 // var btn = document.querySelector('button')
 // var box = document.querySelector("#box")
 
+// btn.addEventListener('click',function(){
+//   var c1 = Math.floor(Math.random() * 256) 
+//   var c2 = Math.floor(Math.random() * 256)
+//   var c3 = Math.floor(Math.random() * 256)
+//   box.style.backgroundColor = `rgb(${c1},${c2},${c3})`;
+  
+// })
+
+
+// CREATIING ELEMENT----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+var btn = document.querySelector('button')
+var main = document.querySelector('main')
+
 btn.addEventListener('click',function(){
-  var c1 = Math.floor(Math.random() * 256) 
+  var div = document.createElement("div")
+  var x = Math.random() * 100
+  var y = Math.random() *100
+  var r = Math.random() * 100
+  var c1 = Math.floor(Math.random() * 256)
   var c2 = Math.floor(Math.random() * 256)
   var c3 = Math.floor(Math.random() * 256)
-  box.style.backgroundColor = `rgb(${c1},${c2},${c3})`;
-  
+
+  div.style.width = "100px";
+  div.style.height = "100px";
+  div.style.position = "absolute";
+  div.style.left = x + "%";
+  div.style.top = y + "%";
+  div.style.transform = `rotate(${r}deg)`
+  div.style.backgroundColor = `rgb(${c1},${c2},${c3})`;
+  main.appendChild(div);
 })
