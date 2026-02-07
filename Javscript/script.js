cd47ce43a8a5d7291da1453c8c41d3bb94623319
Avnish-kaushik <kaushikavi7002@gmail.com>
2026-02-07 17:21:14 +0530

    Like button feature

--- Javscript/script.js
+++ Javscript/script.js
@@ -539,3 +539,15 @@
 //   },num * 100)
 // })
 
+var img = document.querySelector('img')
+var love = document.querySelector('#love')
+
+img.addEventListener('dblclick',function(){
+  love.style.opacity = 1;
+  love.style.color = "red";
+  love.style.transform = "translate(-50%, -50%) scale(1.5)";
+  setTimeout(()=>{
+    love.style.opacity = 0;
+    love.style.transform = "translate(-50%, -50%) scale(0)";
+  },1000) 
+})
