4e175e76c8dd5b0d80cfdab0acb4f39d1e75f4bc
Avnish-kaushik <kaushikavi7002@gmail.com>
2026-06-21 09:36:54 +0530

    React Basics and Folder Structure

---
+++ ReactJs/class1-task/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Section from "./components/Section";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Section />
+    </div>
+  )
+}
+export default App
---
+++ ReactJs/class1-task/src/components/Center.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Center = () => {
+    return (
+        <div className='center'>this is Center</div>
+    )
+}
+
+export default Center
