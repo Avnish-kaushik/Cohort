81f9b0f529829d41115ab29885d55247d4aba3d5
Avnish-kaushik <kaushikavi7002@gmail.com>
2026-06-22 18:51:29 +0530

    Tailwind Basics

---
+++ ReactJs/class2/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex-row align-center justify-center '>
+      <h1 className='text-5xl p-8'>suno, mere veero!</h1>
+    </div>
+  )
+}
+
+export default App
