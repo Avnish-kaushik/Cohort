f4d0c7d784854b5f046b2283aee42c7218348378
Avnish-kaushik <kaushikavi7002@gmail.com>
2026-07-03 19:12:17 +0530

    Two way binding

--- ReactJs/class6/src/App.jsx
+++ ReactJs/class6/src/App.jsx
@@ -1,19 +1,37 @@
 import React from 'react'
 import { useState } from 'react'
 const App = () => {
+  const [name, setname] = useState(" ")
 
-  const submitHandler = (e)=>{
+  const [allUsers, setAllUsers] = useState([])
+
+  const submitHandler = (e) => {
     e.preventDefault();
-    console.log('form not submitted');
-      }
+    const newAllUsers = [...allUsers];
+    newAllUsers.push(name);
+    setAllUsers(newAllUsers);
+    console.log(newAllUsers);
+    setname(" ");
+  }
 
   return (
     <div className='flex items-center justify-center h-screen'>
-      <form className='p-4 bg-gray-100 rounded' onSubmit={submitHandler}>
-        <input className='p-4 border' type="text" placeholder='Enter your name' />
-        <button className='p-4 border text-white
-        bg-blue-500 hover:bg-blue-600 hover:cursor-pointer'>Submit</button>
+      <form className='p-4 bg-gray-100 rounded flex flex-col gap-4'
+        onSubmit={submitHandler}>
+
+        <input className='p-4 border'
+          type="text"
+          placeholder='Enter your name'
+          value={name}
+          required
+          onChange={(e) => {
+            setname(e.target.value);
+          }} />
+
+        <button className='p-4 border text-white bg-blue-500
+        hover:bg-blue-600 hover:cursor-pointer'>Submit</button>
       </form>
+
     </div>
   )
 }
