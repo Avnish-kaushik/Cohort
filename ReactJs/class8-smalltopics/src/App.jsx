ad55e3ae7950ff41488767373adb35d27b957d6a
Avnish-kaushik <kaushikavi7002@gmail.com>
2026-07-07 11:00:33 +0530

    Fragments & props drilling concepts

---
+++ ReactJs/class8-smalltopics/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    // Fragments- khali tag- ek hi parent element return karna zaroori hota hai 
+     
+    <>
+    <h1>Hello</h1>
+    <h2>Kaise ho aap log ?</h2>
+    </>
+  )
+}
+
+export default App
