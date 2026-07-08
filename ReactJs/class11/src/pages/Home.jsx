4d08a377dc0650f5169af792c32b5ca91af77fbb
Avnish-kaushik <kaushikavi7002@gmail.com>
2026-07-08 21:38:06 +0530

    Router basics

---
+++ ReactJs/class11/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-3xl font-semibold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>HOME PAGE</div>
+  )
+}
+
+export default Home
