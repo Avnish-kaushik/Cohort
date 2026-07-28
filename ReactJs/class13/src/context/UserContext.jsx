eb001ddfd854e44be135abd6f49a9366f8451973
Avnish-kaushik <kaushikavi7002@gmail.com>
2026-07-28 19:00:29 +0530

    Context-API-1

---
+++ ReactJs/class13/src/context/UserContext.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { createContext } from 'react'
+
+export const UserDataContext = createContext()
+
+const UserContext = (props) => {
+    const user = 'Avnish'
+
+    return (
+        <div className='context-div'>
+            <UserDataContext.Provider value={user}>
+                {props.children}
+            </UserDataContext.Provider>
+        </div>
+    )
+}
+
+export default UserContext
